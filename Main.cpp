1d1f54a651c25a80fbdd1bb6cfae79e41dd76a93
bilalalmassry <33354606+bilalalmassry@users.noreply.github.com>
2017-11-14 19:11:40 +0200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "Header.h"
+using namespace std;
+int main()
+{
+	int real, img;
+	complex c();
+	cout << "Enter the real part then click ENTER then Enter the img part:/t";
+	cin >> real >> img;
+	c.readcomplex(real,img);
+	system("pause");
+	return 0;
+}
